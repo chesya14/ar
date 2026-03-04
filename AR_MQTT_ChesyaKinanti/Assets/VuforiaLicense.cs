2be47ee08bcfa66ebc520907d88b7e6d311f7f78
atma24 <snowfreze@gmail.com>
2026-03-05 00:50:47 +0700

    v2

---
+++ AR_MQTT_ChesyaKinanti/Assets/VuforiaLicense.cs
@@ -0,0 +1,14 @@
+
+            //This class is auto-generated, please do not overwrite
+            using UnityEngine.Scripting;
+
+            public class VuforiaLicense
+            {
+const string license = "QVJvZXE2RC8vLy8vQUFBQm1VWlhQY2Q5Y1VTdmt0SHZ6VDczTXFCeld2TUpHZ05BZzlrQVVyY0ZCMU5ZV3JlWGpuM0N5cm1XL0loU0c3V0xycDRsZWhiLzNSQ3lrVlVZdnR2UGF6QzZIaTVhcGRIL3laWC9nc1Q3Ry9QNFdxRGkwWHJjWUxMSVJUbHlra2xBbXBFWG9CZUw5SVlIZjRDaXFnak9hYzJYalg2ZXdjeW9RL2wvd0cvSGozSkdzM2p6WkhaRE9KUkFCcVRib21ES2dqQzVhZzlTODRQR1pyYWF0SGwwTHFQb05nOEJQYjcwYUY4a0dQVFgzUGNmTzFqVFh6L3RmWnVSaFRPcXZWd2NHQTZWd0pndXEzRUJJc09BZmFQb1ZrdFdLVWJNajBjTnJOd2JCaGszN3FOc3IxRXJwUDVvVng1alpwVWRSaGQwaFVkMk1UV05TV3FVd3daKzlidk1VTUM2VGZTdjNQbllQQ3Y1emNJR0M3MG0=";
+
+                [Preserve]
+                static string GetLicenseKey()
+                {
+                    return license;
+                }
+            }
